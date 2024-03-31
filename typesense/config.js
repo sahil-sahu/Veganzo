0105aa9643cb3e76b72b6e61664c1084a3e4ee7e
sahil-sahu <sahilku2003@gmail.com>
2024-04-01 03:18:53 +0530

    domain checking

--- typesense/config.js
+++ typesense/config.js
@@ -7,7 +7,7 @@
 const Typesense = require('typesense')
 const client = new Typesense.Client({
     'nodes': [{
-      'host': process.env.NEXT_PUBLIC_TYPESENSE_DOMAIN ?? "localhost", // For Typesense Cloud use xxx.a1.typesense.net
+      'host': process.env.NEXT_PUBLIC_TYPESENSE_DOMAIN || "localhost", // For Typesense Cloud use xxx.a1.typesense.net
       'port': '443',      // For Typesense Cloud use 443
       'protocol': 'https'   // For Typesense Cloud use https
     }],
