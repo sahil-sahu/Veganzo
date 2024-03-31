e2eecfcb491cf835b481c0d9ac787677933e7729
sahil-sahu <sahilku2003@gmail.com>
2024-04-01 03:13:45 +0530

    port 8108->443

--- typesense/config.js
+++ typesense/config.js
@@ -8,7 +8,7 @@ const Typesense = require('typesense')
 const client = new Typesense.Client({
     'nodes': [{
       'host': process.env.NEXT_PUBLIC_TYPESENSE_DOMAIN ?? "localhost", // For Typesense Cloud use xxx.a1.typesense.net
-      'port': '8108',      // For Typesense Cloud use 443
+      'port': '443',      // For Typesense Cloud use 443
       'protocol': 'https'   // For Typesense Cloud use https
     }],
     'apiKey': process.env.NEXT_PUBLIC_TYPESENSE,
